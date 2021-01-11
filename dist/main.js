54ab7d1f00afca1774139874be44622e79479203
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2021-01-11 11:22:28 +0100

    'Dropdown Done'

--- dist/main.js
+++ dist/main.js
@@ -1,33 +1,60 @@
 const menu = document.querySelector('.ham-menu'); 
-        const nav = document.querySelector('.mobile-nav');
-        const dashes = document.querySelectorAll('.dash');
-        console.log(dashes);
+const nav = document.querySelector('.mobile-nav');
+const dashes = document.querySelectorAll('.dash');
+console.log(dashes);
 
-        menu.addEventListener('click', () => {
+menu.addEventListener('click', () => {
 
-            let navOpen = menu.classList.contains('nav-open')
+let navOpen = menu.classList.contains('nav-open')
 
-            if (!navOpen) {
-                nav.style.top = '0'
-                nav.style.marginBottom = '0'
-                menu.classList.remove('nav-close')
-                menu.classList.add('nav-open')
-                dashes[0].style.transform = 'rotate(34deg)'
-                dashes[1].style.display = 'none'
-                dashes[2].style.transform = 'rotate(-34deg)'
+if (!navOpen) {
+    nav.style.top = '0'
+    nav.style.marginBottom = '0'
+    menu.classList.remove('nav-close')
+    menu.classList.add('nav-open')
+    dashes[0].style.transform = 'rotate(34deg)'
+    dashes[1].style.display = 'none'
+    dashes[2].style.transform = 'rotate(-34deg)'
 
-            } 
-            
-            if (navOpen) {
-                nav.style.top = '-100px'
-                nav.style.marginBottom = '-100px'
-                menu.classList.add('nav-close')
-                menu.classList.remove('nav-open')
-                dashes[0].style.transform = 'rotate(0deg)'
-                dashes[1].style.display = 'block'
-                dashes[2].style.transform = 'rotate(-0deg)'
+} 
 
-            }
-            
+if (navOpen) {
+    nav.style.top = '-100px'
+    nav.style.marginBottom = '-100px'
+    menu.classList.add('nav-close')
+    menu.classList.remove('nav-open')
+    dashes[0].style.transform = 'rotate(0deg)'
+    dashes[1].style.display = 'block'
+    dashes[2].style.transform = 'rotate(-0deg)'
 
-            })
+}
+
+})
+
+
+// **********************************
+// ISMAIL's SHIT
+// **********************************
+
+const category__item = document.querySelectorAll('.category__item');
+
+category__item.forEach(function(e) {
+    e.addEventListener('mouseenter', showCatList, {once: true})
+})
+function showCatList(e) {
+    let service_list = e.target.nextElementSibling;
+    let category = e.target.parentElement;
+    console.log('This is ' + service_list);
+    service_list.classList.add('show')
+    category.addEventListener('mouseleave', (e) => {
+        hideCatList(e, category)}, {once: true}
+    )
+}
+
+function hideCatList(e, category) {
+    category.children[0].addEventListener('mouseenter', showCatList, {once: true})
+    category.children[1].classList.remove('show')
+}
+
+
+
